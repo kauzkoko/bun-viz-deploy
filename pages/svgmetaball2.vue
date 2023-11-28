2bc00746f67ff204fb9601eefa7f632405d24907
kauzkoko <kamelkebab@tutanota.com>
2023-11-28 18:21:26 +0100

    feat: svg metaballs logics

---
+++ pages/svgmetaball2.vue
@@ -0,0 +1,63 @@
+<template>
+  <div w-screen h-screen overflow-hidden>
+    <svg
+      width="1920px"
+      height="1080px"
+      class="border-black border-solid border-1px m-5"
+    >
+      <defs>
+        <filter
+          id="metaballFilter"
+          width="400%"
+          x="-150%"
+          height="400%"
+          y="-150%"
+        >
+          <feGaussianBlur
+            id="blurElement"
+            in="SourceGraphic"
+            stdDeviation="20"
+            result="blur"
+          />
+          <feColorMatrix
+            id="colorMatrixElement"
+            in="blur"
+            mode="matrix"
+            values="1 0 0 0  0
+                                                                                     0 1 0 0  0
+                                                                                     0 0 1 0  0
+                                                                                     0 0 0 25 -15"
+            result="matrix"
+          />
+        </filter>
+      </defs>
+      <g filter="url(#metaballFilter)">
+        <circle :cx="800" :cy="y" :r="100" fill="red" />
+        <circle cx="800" cy="500" :r="fakedR" fill="blue" />
+      </g>
+    </svg>
+    <pre text-50px>x: {{ x }}</pre>
+    <pre text-50px>y: {{ y }}</pre>
+    <div bg-blue text-50px>
+      difference between red and blue is: {{ difference }}
+    </div>
+    <div text-50px>
+      if r is 100, then r^2*PI = A: {{ Math.pow(100, 2) * Math.PI }}<br />
+    </div>
+  </div>
+</template>
+
+<script setup>
+const { x, y } = useMouse();
+const difference = computed(() => Math.abs(y.value - 500));
+watch(x, () => {
+  console.log("difference on y between red and blue is: ", y.value - 500);
+});
+const fakedR = computed(() => {
+  if (difference.value < 215) {
+    return 100 + 215 / 2 - difference.value / 2;
+  } else {
+    return 100;
+  }
+});
+</script>
