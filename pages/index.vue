56c74847672609a4282dfbc63ce70388d7d87d1c
kauzkoko <kamelkebab@tutanota.com>
2023-12-17 16:57:58 +0100

    refactor: cleanup

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,22 @@
 <template>
-  <div>
-    <div v-for="route in routes">
+  <div class="p-5 text-white">
+    <h1 class="text-40px font-bold text-white uppercase">
+      Data Visualisation Module – ZHdK x Crowther Lab
+    </h1>
+    <h2 class="text-30px mt-10 mb-2 font-bold">
+      Interactive prototype as shown in final presentation, designed for 4K
+      touchscreen in fullscreen, <br />on smaller screens use browser zoom or
+      responsive mode in devtools to recreate the experience
+    </h2>
+    <NuxtLink to="/Final">Final</NuxtLink>
+    <h2 class="text-30px mt-10 mb-2 font-bold">
+      Prototypes / experiments / dev pages
+    </h2>
+    <div v-for="route in routes" class="mb-1">
       <NuxtLink
-        class="text-20px"
         :to="route.path"
-        v-if="route.name !== 'index'"
-        >{{
-          route.name.split("-")[0] + "/" + route.name.split("-")[1]
-        }}</NuxtLink
+        v-if="route.name !== 'index' && route.name !== 'Final'"
+        >{{ route.name.split("-")[0] }}</NuxtLink
       >
     </div>
   </div>
@@ -16,3 +25,10 @@
 <script setup>
 const routes = useRouter().getRoutes();
 </script>
+
+<style scoped>
+a {
+  font-size: 30px;
+  color: white;
+}
+</style>
